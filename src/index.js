451154a027c29a2ff5f60fbb5cbad2e82d3c5e22
Kobe Mertens <jacobignaas.mertens@ugent.be>
2019-02-24 16:12:30 +0100

    added helloworld

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class HelloWorld extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+        }
+    }
+
+    increaseCount(){
+        const count = this.state.count + 1;
+        this.setState({count: count});
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Hello, World!</p>
+                <Counter value={this.state.count}></Counter>
+                <IncreaseButton
+                    increaseCount={() => this.increaseCount()}
+                />
+            </div>
+        )
+    }
+}
+
+function IncreaseButton(props){
+    return (
+        <button onClick={props.increaseCount}>Press me</button>
+    )
+}
+
+function Counter(props) {
+    return (<div><p>{props.value}</p></div>)
+}
+
+ReactDOM.render(<HelloWorld />,
+    document.getElementById("root"));
